4bf5f9dd489eafe8d49c9f5ab04796e1c5c709c5
Yazdani <yaz4dani@gmail.com>
2019-04-28 01:52:27 +0600

    second commit

--- app/src/main/java/yazdaniscodelab/topnoteapp/HomeActivity.java
+++ app/src/main/java/yazdaniscodelab/topnoteapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package yazdaniscodelab.topnoteapp;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -254,6 +257,23 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
